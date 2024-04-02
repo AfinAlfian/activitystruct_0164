62291be80dfe1682f0cb4299498edb092f33b88e
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-04-02 11:43:21 +0700

    menambahkan variabel struct detail alamat

--- Struct4/Struct4.cpp
+++ Struct4/Struct4.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 
+struct DetailAlamat
+{
+    string desa;
+    string kota;
+};
+
 struct Mahasiswa
 {
     string nim;
     string nama;
-    string alamat;
+    DetailAlamat alamat;
     string umur;
 };
 
